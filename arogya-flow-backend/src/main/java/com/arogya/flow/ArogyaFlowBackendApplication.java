3356af054b87179d92d6e94df8623e09e7f11b34
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-30 13:45:54 +0530

    Start backend development

---
+++ arogya-flow-backend/src/main/java/com/arogya/flow/ArogyaFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arogya.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArogyaFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArogyaFlowBackendApplication.class, args);
+	}
+
+}
---
+++ arogya-flow-backend/src/test/java/com/arogya/flow/ArogyaFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arogya.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArogyaFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
