19a5bfb9d31396a8285f1a8714f6f6a561b8507f
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-31 15:30:11 +0530

    Create Token Entity

---
+++ arogya-flow-backend/src/main/java/com/arogya/flow/entity/Token.java
@@ -0,0 +1,51 @@
+package com.arogya.flow.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "tokens",
+        indexes = {
+                @Index(name = "idx_token_number", columnList = "tokenNumber"),
+                @Index(name = "idx_slot_status", columnList = "status")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String tokenNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "slot_id", nullable = false)
+    private Slot slot;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TokenSource source;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TokenStatus status;
+
+    @Column(nullable = false)
+    private Integer priority;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
