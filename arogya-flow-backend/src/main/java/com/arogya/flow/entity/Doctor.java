02b42653928cd244b29944010b7c23551cbb3f04
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-31 13:53:08 +0530

    Updated Doctor Entity

--- arogya-flow-backend/src/main/java/com/arogya/flow/entity/Doctor.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/entity/Doctor.java
@@ -1,4 +1,39 @@
 package com.arogya.flow.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "doctors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String specialization;
+
+    @Column(nullable = false)
+    private Integer maxTokenPerSlot;
+
+    @Column(nullable = false)
+    private LocalTime availableFrom;
+
+    @Column(nullable = false)
+    private LocalTime availableTo;
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Slot> slots;
 }
