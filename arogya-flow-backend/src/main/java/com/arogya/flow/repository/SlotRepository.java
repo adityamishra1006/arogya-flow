4a5498e7ddb3d81426beb325583861652a0ae305
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-01 21:39:04 +0530

    Create SlotRepo

---
+++ arogya-flow-backend/src/main/java/com/arogya/flow/repository/SlotRepository.java
@@ -0,0 +1,9 @@
+package com.arogya.flow.repository;
+
+import com.arogya.flow.entity.Slot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SlotRepository extends JpaRepository<Slot, Long> {
+}
