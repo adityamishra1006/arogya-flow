09429246fa46918b06bc181c931e32be14043207
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-03 15:52:39 +0530

    Update SlotRepositiory

--- arogya-flow-backend/src/main/java/com/arogya/flow/repository/SlotRepository.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/repository/SlotRepository.java
@@ -1,9 +1,15 @@
 package com.arogya.flow.repository;
 
 import com.arogya.flow.entity.Slot;
+import com.arogya.flow.entity.enums.SlotStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+    List<Slot> findByDoctorIdAndSlotDate(Long doctorId, LocalDate slotDate);
+    List<Slot> findByDoctorIdAndSlotDateAndStatus(Long doctorId, LocalDate slotDate, SlotStatus status);
 }
