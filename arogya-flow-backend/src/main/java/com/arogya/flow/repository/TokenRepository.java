28a523148debc2211f57eaf072b1287cb500a0db
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-03 15:53:02 +0530

    Update TokenRepository

--- arogya-flow-backend/src/main/java/com/arogya/flow/repository/TokenRepository.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/repository/TokenRepository.java
@@ -1,10 +1,14 @@
 package com.arogya.flow.repository;
 
 import com.arogya.flow.entity.Token;
+import com.arogya.flow.entity.enums.TokenStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
-    
+    List<Token> findBySlotIdAndStatus(Long slotId, TokenStatus status);
+    boolean existsByTokenNumber(String tokenNumber);
 }
