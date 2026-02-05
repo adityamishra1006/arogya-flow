3b8a0dc4c353aa286e2eb24a402ff2f43d56fd0d
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-05 16:45:35 +0530

    Update TokenRequestDTO
    Added some validation

--- arogya-flow-backend/src/main/java/com/arogya/flow/dto/TokenRequestDTO.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/dto/TokenRequestDTO.java
@@ -1,13 +1,16 @@
 package com.arogya.flow.dto;
 
 import com.arogya.flow.entity.enums.TokenSource;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class TokenRequestDTO {
+    @NotNull
     private Long doctorId;
     private Long slotId;
+    @NotNull
     private TokenSource source;
 }
