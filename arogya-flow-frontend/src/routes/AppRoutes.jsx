19cfabf7df26922d1baa68ac291c610a985a062f
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-09 20:35:46 +0530

    refactor: changes made such that the file can route better

--- arogya-flow-frontend/src/routes/AppRoutes.jsx
+++ arogya-flow-frontend/src/routes/AppRoutes.jsx
@@ -1,19 +1,16 @@
-import {Route, Routes} from "react-router-dom";
-import Dashboard from "../pages/Dashboard.jsx"
-import Doctors from "../pages/Doctors.jsx"
-import NotFound from "../pages/NotFound.jsx"
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard.jsx";
+import Doctors from "../pages/Doctors.jsx";
+import Slots from "../pages/DoctorSlots.jsx";
+import NotFound from "../pages/NotFound.jsx";
 
-export default function AppRoutes(){
+export default function AppRoutes() {
     return (
         <Routes>
-            {/*Main Dashboard*/}
             <Route path="/" element={<Dashboard />} />
-
-            {/*Doctors & OPD Units*/}
             <Route path="/doctors" element={<Doctors />} />
-
-            {/*Go Back*/}
+            <Route path="/doctors/:doctorId/slots" element={<Slots />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
-    )
+    );
 }
