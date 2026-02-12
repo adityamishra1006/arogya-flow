e1fef25469982704f2b71279c8e76b3cf1c7f248
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-12 16:25:47 +0530

    feat: create modal for booking slot

---
+++ arogya-flow-frontend/src/components/slots/BookSlotModal.jsx
@@ -0,0 +1,57 @@
+import {useState} from "react";
+import Modal from "../common/Modal.jsx";
+import Button from "../common/Button.jsx";
+
+export default function BookSlotModal({ isOpen, onClose, onSubmit, slot }){
+    const [patientName, setPatientName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = () => {
+        if(!patientName || !email){
+            alert("Please enter a valid details");
+            return;
+        }
+
+        onSubmit({
+            slotId: slot.id,
+            patientName,
+            email,
+        });
+
+        setPatientName("");
+        setEmail("");
+    };
+
+    return(
+        <Modal
+            isOpen={isOpen} onClose={onClose} title="Book slots"
+            footer={
+                <>
+                    <Button variant="secondary" onClick={onClose}>
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSubmit}>
+                        Submit
+                    </Button>
+                </>
+            }
+        >
+
+            <div className="space-y-3">
+                <input
+                    className="w-full border rounded p-2"
+                    placeholder="Patient Name"
+                    value={patientName}
+                    onChange={(e) => setPatientName(e.target.value)}
+                />
+                <input
+                    className="w-full border rounded p-2"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+
+        </Modal>
+    )
+}
