8d59b278b2626fe14f53cdea8e2f3c29cbfe5c65
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-12 16:23:17 +0530

    feat: create appointment repository

---
+++ arogya-flow-backend/src/main/java/com/arogya/flow/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.arogya.flow.repository;
+
+import com.arogya.flow.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
