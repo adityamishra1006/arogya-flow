0a13239774d8dc8a76fd270d4a9d8cbadfdbadb7
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-12 16:24:25 +0530

    feat: create appointment service for booking appointment

---
+++ arogya-flow-backend/src/main/java/com/arogya/flow/service/AppointmentService.java
@@ -0,0 +1,52 @@
+package com.arogya.flow.service;
+
+import com.arogya.flow.dto.AppointmentDTO;
+import com.arogya.flow.dto.SlotBookingRequestDTO;
+import com.arogya.flow.entity.Appointment;
+import com.arogya.flow.entity.Slot;
+import com.arogya.flow.entity.enums.SlotStatus;
+import com.arogya.flow.exception.ResourceNotFoundException;
+import com.arogya.flow.repository.AppointmentRepository;
+import com.arogya.flow.repository.SlotRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService {
+    private final SlotRepository slotRepository;
+    private final AppointmentRepository appointmentRepository;
+    private final SlotService slotService;
+
+    @Transactional
+    public AppointmentDTO bookSlot(SlotBookingRequestDTO request){
+        Slot slot = slotRepository.findById(request.getSlotId())
+                .orElseThrow(()->
+                            new ResourceNotFoundException("Slot Not Found")
+                        );
+
+        if(slot.getStatus() != SlotStatus.OPEN){
+            throw new IllegalStateException("Slot is already booked or closed");
+        }
+
+        Appointment appointment = Appointment.builder()
+                .patientName(request.getPatientName())
+                .email(request.getEmail())
+                .slot(slot)
+                .build();
+
+        Appointment saved = appointmentRepository.save(appointment);
+
+        slot.setStatus(SlotStatus.BOOKED);
+        slotRepository.save(slot);
+
+        return new AppointmentDTO(
+                saved.getId(),
+                saved.getPatientName(),
+                saved.getEmail(),
+                slot.getId()
+        );
+    }
+}
