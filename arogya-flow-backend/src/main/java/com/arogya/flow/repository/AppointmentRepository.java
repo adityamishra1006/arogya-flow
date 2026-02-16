e9b696ee0d5879cf322c2c72ffa831d8fe48fe18
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-16 15:58:18 +0530

    Created a function to count the number of Appointments

--- arogya-flow-backend/src/main/java/com/arogya/flow/repository/AppointmentRepository.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/repository/AppointmentRepository.java
@@ -2,8 +2,25 @@ package com.arogya.flow.repository;
 
 import com.arogya.flow.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @Query("""
+        SELECT COUNT(a)
+        FROM Appointment a
+        WHERE 
+            a.slot.slotDate > :today
+            OR (a.slot.slotDate = :today AND a.slot.endTime > :now)
+    """)
+    long countUpcomingAppointments(
+            @Param("today") LocalDate today,
+            @Param("now") LocalTime now
+    );
 }
