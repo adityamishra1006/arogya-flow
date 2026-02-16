5f348ae0496252c0cd2f41507966f3b047d59f12
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-16 15:57:23 +0530

    updated DashboardController for more detailed dashboard view

--- arogya-flow-backend/src/main/java/com/arogya/flow/controller/DashBoardController.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/controller/DashBoardController.java
@@ -1,7 +1,9 @@
 package com.arogya.flow.controller;
 
 import com.arogya.flow.dto.DashboardStatsDTO;
+import com.arogya.flow.entity.enums.SlotStatus;
 import com.arogya.flow.entity.enums.TokenStatus;
+import com.arogya.flow.repository.AppointmentRepository;
 import com.arogya.flow.repository.DoctorRepository;
 import com.arogya.flow.repository.SlotRepository;
 import com.arogya.flow.repository.TokenRepository;
@@ -9,8 +11,12 @@ import com.arogya.flow.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,28 +25,35 @@ public class DashBoardController {
     private final DoctorRepository doctorRepository;
     private final SlotRepository slotRepository;
     private final TokenRepository tokenRepository;
+    private final AppointmentRepository appointmentRepository;
 
     @GetMapping("/stats")
     public ResponseEntity<DashboardStatsDTO> getDashBoardStats(){
+        LocalDate today = LocalDate.now();
+        LocalTime now = LocalTime.now();
+
         long totalDoctors = doctorRepository.count();
         long totalSlots = slotRepository.count();
-        long totalTokens = tokenRepository.count();
+        long upcomingAppiontment = appointmentRepository.countUpcomingAppointments(today, now);
 
-        long activityTokens = tokenRepository
-                .findAll()
-                .stream()
-                .filter(token ->
-                        token.getStatus() == TokenStatus.CREATED ||
-                                token.getStatus() == TokenStatus.SERVING
-                )
-                .count();
+        long bookedSlots = slotRepository.countByStatus(SlotStatus.BOOKED);
+        long openSlots = slotRepository.countByStatus(SlotStatus.OPEN);
 
         DashboardStatsDTO stats = new DashboardStatsDTO(
                 totalDoctors,
                 totalSlots,
-                totalTokens,
-                activityTokens
+                bookedSlots,
+                openSlots,
+                upcomingAppiontment
         );
         return ResponseEntity.ok(stats);
+    }
+
+    @PostMapping("/reset")
+    public ResponseEntity<String> resetDashboard(){
+        appointmentRepository.deleteAll();
+        slotRepository.deleteAll();
+
+        return ResponseEntity.ok("Dashboard reset successfully");
     }
 }
