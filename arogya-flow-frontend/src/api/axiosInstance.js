4aa4261ee9ca9037b8b81ba0cb35a30762ff19e9
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-19 19:10:14 +0530

    Updated axiosInstance

--- arogya-flow-frontend/src/api/axiosInstance.js
+++ arogya-flow-frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:8080/api",
-    headers:{
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
         "Content-Type": "application/json",
     },
     timeout: 10000,
@@ -11,7 +11,10 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
     (response) => response,
     (error) => {
-        console.error("API Error: ", error);
+        console.error(
+            "API Error:",
+            error.response?.data || error.message
+        );
         return Promise.reject(error);
     }
 );
