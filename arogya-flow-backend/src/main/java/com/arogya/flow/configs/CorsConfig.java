c4bce0a8939af0a042c173ef36c9e91bc7af27e1
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-19 19:16:19 +0530

    Update CorsConfig and extended the timeout

--- arogya-flow-backend/src/main/java/com/arogya/flow/configs/CorsConfig.java
+++ arogya-flow-backend/src/main/java/com/arogya/flow/configs/CorsConfig.java
@@ -3,18 +3,21 @@ package com.arogya.flow.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry){
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",          // local dev
+                                "https://arogya-flow.vercel.app"  // deployed frontend
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
